d44e847324d5c4e3089a81bf211ca86f6b0bc27a
Kelompok1pjjda2 <135815172+Kelompok1pjjda2@users.noreply.github.com>
2023-07-03 15:56:48 +0700

    Add files via upload

---
+++ UJICOBA.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor,ExtraTreesRegressor
+from sklearn.model_selection import validation_curve, LeaveOneOut, train_test_split, cross_val_score
+from sklearn.model_selection import cross_validate, KFold, cross_val_score, RandomizedSearchCV, GridSearchCV
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+from sklearn.preprocessing import OneHotEncoder 
+from matplotlib import pyplot
+import pickle
+import math
+
+
+st.write("Ini cuma uji coba")
